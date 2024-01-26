6efc8978b12dea0d37f325daccfab9d08eb57d51
GreenGraceN <935066631@qq.com>
2024-01-26 16:55:32 +0100

    Update yuan-ning-a3.2R.R

--- yuan-ning-a3.2R.R
+++ yuan-ning-a3.2R.R
@@ -10,21 +10,28 @@
 
 # Q3.2R.1 -------------------------------------------------------------
 # Answer
-
 remind_me <- function() {
   return("Remember to improve your CV for internship!")
 }
 
+cheat <- function(exercise_number) {
+  answers <- list(
+    "3.1.1" = "Q3.1.1",
+    "3.1.2" = "Q3.1.2",
+    "3.1.3" = "Q3.1.3"
+  )
+  return(answers[[exercise_number]])
+}
+# Run this in the R console
+install.packages("devtools")
+devtools::create("E:/ny/2024/Programming/week3/package_name")
 
 
 # Q3.2R.2 -------------------------------------------------------------
 # Answer
+
 make_art <- function(seed = NULL) {
   if (!is.null(seed)) set.seed(seed)
   # Random art generation code goes here
   plot(runif(10), runif(10), col = rainbow(10), pch = 19, cex = runif(10))
 }
-
-
-# Q3.2R.3 -------------------------------------------------------------
-# Answer
